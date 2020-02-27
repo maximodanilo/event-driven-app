85c51a7f4c978a9ca200902b355c04ca5ea95542
Danilo Almeida <Danilo.Almeida@cmic.ca>
2020-02-26 21:45:40 -0500

    Emitting event

--- src/main/java/com/dalmeida/eventdrivenappdemo/EventDrivenAppDemoApplication.java
+++ src/main/java/com/dalmeida/eventdrivenappdemo/EventDrivenAppDemoApplication.java
@@ -1,38 +1,40 @@
 package com.dalmeida.eventdrivenappdemo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.Poller;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
 public class EventDrivenAppDemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(EventDrivenAppDemoApplication.class);
+	private List<String> names = Arrays.asList("Donald", "Theresa", "Vladimir", "Angela", "Emmanuel", "Shinzō",
+			"Jacinda", "Kim");
+	private List<Long> amounts = Arrays.asList(10L, 100L, 1000L, 10000L, 100000L, 1000000L, 10000000L, 100000000L,
+			100000000L);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventDrivenAppDemoApplication.class, args);
+		log.info("The Loansource Application has started...");
 	}
 
-	@StreamListener(Sink.INPUT)
-	public void handle(Person person) {
-		System.out.println("Received: " + person);
-	}
-
-	public static class Person {
-		private String name;
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String toString() {
-			return this.name;
-		}
+	@Bean
+	public Supplier<Loan> supplyLoan() {
+		return () -> {
+			String rName = names.get(new Random().nextInt(names.size()));
+			Long rAmount = amounts.get(new Random().nextInt(amounts.size()));
+			Loan loan = new Loan(UUID.randomUUID().toString(), rName, rAmount);
+			log.info("{} {} for ${} for {}", loan.getStatus(), loan.getUuid(), loan.getAmount(), loan.getName());
+			return loan;
+		};
 	}
 
 }
---
+++ src/main/java/com/dalmeida/eventdrivenappdemo/Statuses.java
@@ -0,0 +1,9 @@
+package com.dalmeida.eventdrivenappdemo;
+
+
+/**
+ * This Enum defines the valid states for a Loan including approved and rejected.
+ */
+public enum Statuses {
+  APPROVED, DECLINED, PENDING, REJECTED
+}
